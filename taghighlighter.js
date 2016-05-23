af99cdd1141f4d3a5552d043bd785e5300bec56a
thedom85 <thedom85@users.noreply.github.com>
2016-05-23 09:13:25 +0200

    Update taghighlighter.js

--- taghighlighter.js
+++ taghighlighter.js
@@ -1,9 +1,9 @@
 
-document.addEventListener('DOMContentLoaded', function() {
-		var all = document.body.getElementsByTagName("*");
-		var colors = [ "blue","blue", "red", "yellow", "green", "grey" ];
-		for(var count =0; count < all.length; ++count) {
-			item = all[count];
-			item.style.outline = "thick solid "+colors[(Math.floor(Math.random() * (5 - 1 + 1)) + 1)];
-		}
+document.addntListener('DOMContentLoaded', function() {
+	var all = document.body.getElementsByTagName("*");
+	var colors = [ "blue","blue", "red", "yellow", "green", "grey" ];
+	for(var count =0; count < all.length; ++count) {
+		item = all[count];
+		item.style.outline = "thick solid "+colors[(Math.floor(Math.random() * (5 - 1 + 1)) + 1)];
+	}
 });
