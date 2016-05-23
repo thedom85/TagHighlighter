a4e96b576056295db0648839cbcfa9e457955a24
thedom85 <thedom85@users.noreply.github.com>
2016-05-23 17:26:28 +0200

    Add Theme
    Add Theme Parameter

--- taghighlighter.js
+++ taghighlighter.js
@@ -5,7 +5,7 @@ function Taghighlighter() {
 
 
 Taghighlighter.prototype.Hlighter = function(tagName,theme){
-
+	if(theme =="random"){
 		if(!tagName)tagName="*";
 		var all = document.body.getElementsByTagName(tagName);
 		var colors = [ "blue","blue", "red", "yellow", "green", "grey" ];
@@ -13,7 +13,7 @@ Taghighlighter.prototype.Hlighter = function(tagName,theme){
 				item = all[count];
 				item.style.outline = "thick solid "+colors[(Math.floor(Math.random() * (5 - 1 + 1)) + 1)];
 			}
-			
+	}		
 }
 
 
