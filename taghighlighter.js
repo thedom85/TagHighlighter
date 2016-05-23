9292ab01eb73585f6953cc90a77176a6372553e8
thedom85 <thedom85@users.noreply.github.com>
2016-05-24 00:38:32 +0200

    Add Theme easy
    Add Theme easy

--- taghighlighter.js
+++ taghighlighter.js
@@ -5,15 +5,24 @@ function Taghighlighter() {
 
 
 Taghighlighter.prototype.Hlighter = function(tagName,theme){
-	if(theme =="random"){
-		if(!tagName)tagName="*";
+	if(!tagName)tagName="*";
+	var all = document.body.getElementsByTagName(tagName);
+	if(theme =="random")
+	{
 		var all = document.body.getElementsByTagName(tagName);
 		var colors = [ "blue","blue", "red", "yellow", "green", "grey" ];
 			for(var count =0; count < all.length; ++count) {
 				item = all[count];
 				item.style.outline = "thick solid "+colors[(Math.floor(Math.random() * (5 - 1 + 1)) + 1)];
 			}
-	}		
+	} 
+	else if (theme =="easy") 
+	{
+		for(var count =0; count < all.length; ++count) {
+			item = all[count];
+			item.style.outline = "thick solid "+"yellow";
+		}
+	}
 }
 
 
