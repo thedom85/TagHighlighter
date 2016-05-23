ac9b091ed392226796e55c412300230f54a1046a
thedom85 <thedom85@users.noreply.github.com>
2016-05-23 17:23:15 +0200

    Create  function Taghighlighter
    Create  function Taghighlighter

--- taghighlighter.js
+++ taghighlighter.js
@@ -1,9 +1,22 @@
 
-document.addntListener('DOMContentLoaded', function() {
-	var all = document.body.getElementsByTagName("*");
-	var colors = [ "blue","blue", "red", "yellow", "green", "grey" ];
-	for(var count =0; count < all.length; ++count) {
-		item = all[count];
-		item.style.outline = "thick solid "+colors[(Math.floor(Math.random() * (5 - 1 + 1)) + 1)];
-	}
-});
+function Taghighlighter() {
+}
+
+
+
+Taghighlighter.prototype.Hlighter = function(tagName,theme){
+
+		if(!tagName)tagName="*";
+		var all = document.body.getElementsByTagName(tagName);
+		var colors = [ "blue","blue", "red", "yellow", "green", "grey" ];
+			for(var count =0; count < all.length; ++count) {
+				item = all[count];
+				item.style.outline = "thick solid "+colors[(Math.floor(Math.random() * (5 - 1 + 1)) + 1)];
+			}
+			
+}
+
+
+if (typeof taghighlighter == "undefined") {
+    var taghighlighter = new Taghighlighter();
+}
